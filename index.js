3e52c0d010e70370990544c1fb892dc55430da1d
anmoln <sirkinggreat@gmail.com>
2022-06-06 00:54:50 +0530

    1

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const uri = process.env.URI
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 // Default
 
 app.get('/', (req, res) => { res.send('Hello from Express!')})
